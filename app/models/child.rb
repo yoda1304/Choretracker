696976d81637e5f60748165097c7f338373d61c1
Jonathan <yoda1304@gmail.com>
2021-02-08 01:55:49 -0500

    made and tested child

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,19 @@
 class Child < ApplicationRecord
+
+    has_many :chores
+    has_many :tasks, through: :tasks
+
+    validates_presence_of(:first_name)
+    validates_presence_of(:last_name)
+
+    scope :alphabetical, -> {order('last_name, first_name')}
+
+    scope :active, -> {where(active: true)}
+
+    def name
+
+        first_name + last_name
+
+    end
     
 end
